a294eebd674573da2fd8105f7b4778eb060cbbc1
HuyenNguyen0312 <2251330018@ut.edu.vn>
2025-06-28 10:16:42 +0700

    Upload new code version

--- Web/shop_quanao.sql
+++ Web/shop_quanao.sql
@@ -39,10 +39,10 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Hoodie', '2021-07-07 11:50:15', '2022-11-06 15:07:28'),
-(2, 'T-shirt', '2021-07-07 11:50:15', '2022-11-06 15:03:28'),
-(3, 'Shorts', '2021-07-07 11:50:15', '2022-11-06 15:57:28'),
-(4, 'Trousers', '2021-07-13 10:57:52', '2022-11-06 15:14:29');
+(1, 'Hoodie', '2025-05-07 11:50:15', '2025-06-06 15:07:28'),
+(2, 'T-shirt', '2025-05-07 11:50:15', '2025-06-06 15:03:28'),
+(3, 'Shorts', '2025-05-07 11:50:15', '2025-06-06 15:57:28'),
+(4, 'Trousers', '2025-05-13 10:57:52', '2025-06-06 15:14:29');
 
 -- --------------------------------------------------------
 
@@ -62,9 +62,9 @@ CREATE TABLE `collections` (
 --
 
 INSERT INTO `collections` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'One Piece', '2023-12-26 18:08:52', '2023-12-26 18:08:52'),
-(2, 'The Spring', '2023-12-26 18:08:52', '2023-12-26 18:08:52'),
-(3, 'Liliwyun', '2023-12-26 18:08:52', '2023-12-26 18:08:52');
+(1, 'One Piece', '2025-05-26 18:08:52', '2025-05-26 18:08:52'),
+(2, 'The Spring', '2025-05-26 18:08:52', '2025-05-26 18:08:52'),
+(3, 'Liliwyun', '2025-05-26 18:08:52', '2025-05-26 18:08:52');
 
 -- --------------------------------------------------------
 
@@ -87,7 +87,7 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`id`, `hoten`, `email`, `message_contact`, `id_user`, `created_at`, `updated_at`) VALUES
-(1, 'user', 'user@gmail.com', 'Tôi cần hỗ trợ', 2, '2023-12-27 22:39:08', '2023-12-27 22:39:08');
+(1, 'user', 'user@gmail.com', 'Tôi cần hỗ trợ', 2, '2025-05-27 22:39:08', '2025-05-27 22:39:08');
 
 -- --------------------------------------------------------
 
@@ -110,17 +110,17 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `fullname`, `phone_number`, `email`, `address`, `note`, `order_date`) VALUES
-(163, 'ninnin_1', '12345678', 'admin@gmail.com', 'Tphcm', 'DC x OP Luffy Raglan Hoodie - Multicolor', '2022-11-06 22:39:49'),
-(164, 'ninnin_2', '12345678', 'admin@gmail.com', 'HaNoi', 'DC x OP Brook Hoodie - Blue', '2022-11-06 22:40:51'),
-(165, 'ninnin_4', '12345678', 'admin@gmail.com', 'CanTho', 'DC x OP Chopper Hoodie - Black', '2022-11-06 22:44:20'),
-(166, 'ninnin_3', '12345678', 'admin@gmail.com', 'NhaTrang', 'Hoodie - Black', '2022-11-06 22:47:24'),
-(172, 'ninnin', '0866206976', 'testweb@gmail.com', 'Tphcm', 'ok', '2023-12-23 12:11:03'),
-(173, 'donhang', '0866206976', 'testweb@gmail.com', 'Tphcm', '', '2023-12-24 05:54:55'),
-(174, 'user_test', '0934373832', 'user@gmail.com', 'TPHCM', 'Thanh toán khi nhận hàng', '2023-12-24 14:00:20'),
-(175, 'hehe', '2343333444', 'testweb@gmail.com', 'Tphcm', 'Thanh toán khi nhận hàng', '2023-12-27 17:13:36'),
-(176, 'hehe', '0866206976', 'testweb@gmail.com', 'Tphcm', '', '2023-12-27 17:14:16'),
-(177, 'chuchu', '2343333444', 'luo63332@nezid.com', 'Tphcm', '', '2023-12-27 17:14:46'),
-(178, 'test123', '0866206976', 'testweb@gmail.com', 'Tphcm', '', '2023-12-28 04:39:16');
+(163, 'ninnin_1', '12345678', 'admin@gmail.com', 'Tphcm', 'DC x OP Luffy Raglan Hoodie - Multicolor', '2025-05-06 22:39:49'),
+(164, 'ninnin_2', '12345678', 'admin@gmail.com', 'HaNoi', 'DC x OP Brook Hoodie - Blue', '2025-05-06 22:40:51'),
+(165, 'ninnin_4', '12345678', 'admin@gmail.com', 'CanTho', 'DC x OP Chopper Hoodie - Black', '2025-05-06 22:44:20'),
+(166, 'ninnin_3', '12345678', 'admin@gmail.com', 'NhaTrang', 'Hoodie - Black', '2025-05-06 22:47:24'),
+(172, 'ninnin', '0866206976', 'testweb@gmail.com', 'Tphcm', 'ok', '2025-05-23 12:11:03'),
+(173, 'donhang', '0866206976', 'testweb@gmail.com', 'Tphcm', '', '2025-05-24 05:54:55'),
+(174, 'user_test', '0934373832', 'user@gmail.com', 'TPHCM', 'Thanh toán khi nhận hàng', '2025-05-24 14:00:20'),
+(175, 'hehe', '2343333444', 'testweb@gmail.com', 'Tphcm', 'Thanh toán khi nhận hàng', '2025-05-27 17:13:36'),
+(176, 'hehe', '0866206976', 'testweb@gmail.com', 'Tphcm', '', '2025-05-27 17:14:16'),
+(177, 'chuchu', '2343333444', 'luo63332@nezid.com', 'Tphcm', '', '2025-05-27 17:14:46'),
+(178, 'test123', '0866206976', 'testweb@gmail.com', 'Tphcm', '', '2025-05-28 04:39:16');
 
 -- --------------------------------------------------------
 
@@ -177,21 +177,21 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `title`, `price`, `number`, `thumbnail`, `thumbnail_1`, `thumbnail_2`, `thumbnail_3`, `thumbnail_4`, `thumbnail_5`, `content`, `id_category`, `id_sanpham`, `created_at`, `updated_at`) VALUES
-(1, 'DC x OP Luffy Raglan Hoodie - Multicolor Special', 750000, 10, 'uploads/p1.png', 'uploads/p2.png', 'uploads/p3.png', 'uploads/p4.png', 'uploads/p5.png', 'uploads/p6.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 1, '2022-10-30 10:37:18', '2023-12-27 15:24:13'),
-(2, 'DC x OP Brook Hoodie - Blue', 699000, 20, 'uploads/p7.png', 'uploads/p8.png', 'uploads/p9.png', 'uploads/p12.png', 'uploads/p11.png', 'uploads/p10.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 1, '2022-10-30 15:11:49', '2022-10-30 15:11:49'),
-(3, 'DC x OP Luffy Attack T-shirt - White', 420000, 20, 'uploads/p13.png', 'uploads/p18.png', 'uploads/p15.png', 'uploads/p14.png', 'uploads/p17.png', 'uploads/p18.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 2, 1, '2022-10-30 17:31:22', '2023-12-27 12:09:25'),
-(4, 'DirtyCoins Don\'t Test Us Hoodie - Black', 648000, 20, 'uploads/p31.png', 'uploads/p32.png', 'uploads/p33.png', 'uploads/p36.png', 'uploads/p35.png', 'uploads/p34.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 2, '2022-10-30 21:59:06', '2022-10-30 21:59:06'),
-(5, 'DC x OP Chopper Hoodie - Black', 699000, 10, 'uploads/p67.png', 'uploads/p71.png', 'uploads/p69.png', 'uploads/p68-1.png', 'uploads/p70.png', 'uploads/p72.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 1, '2022-10-30 22:10:40', '2022-10-30 22:10:40'),
-(6, 'DirtyCoins x LilWuyn Logo Shorts - Black', 450000, 10, 'uploads/p25.png', 'uploads/p26.png', 'uploads/p27.png', 'uploads/p28.png', 'uploads/p29.png', 'uploads/p30.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 3, 3, '2022-10-31 12:37:46', '2023-12-27 08:41:12'),
-(7, 'Dico Wavy Shorts - Black', 490000, 20, 'uploads/p55.png', 'uploads/p56.png', 'uploads/p57.png', 'uploads/p58.png', 'uploads/p59.png', 'uploads/p60.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 3, 2, '2022-10-31 12:40:58', '2022-10-31 12:40:58'),
-(8, 'Shorts - Green', 450000, 20, 'uploads/p61.png', 'uploads/p62.png', 'uploads/p63.png', 'uploads/p64.png', 'uploads/p65.png', 'uploads/p66.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 3, 2, '2022-10-31 12:43:18', '2022-10-31 12:43:18'),
-(9, 'Cargo Pants - Black', 620000, 15, 'uploads/p43.png', 'uploads/p44.png', 'uploads/p45.png', 'uploads/p46.png', 'uploads/p47.png', 'uploads/p48.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 4, 2, '2022-10-31 12:55:56', '2022-10-31 12:55:56'),
-(10, 'DirtyCoins Dico Starry Jogger Pants - Red', 490000, 20, 'uploads/p49-1.png', 'uploads/p50.png', 'uploads/p51.png', 'uploads/p52.png', 'uploads/p53.png', 'uploads/p54.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 4, 2, '2022-10-31 12:59:42', '2022-10-31 12:59:42'),
-(11, 'DC x OP Logo Print Khaki Pants - Black', 620000, 10, 'uploads/p73.png', 'uploads/p74.png', 'uploads/p75.png', 'uploads/p76.png', 'uploads/p77.png', 'uploads/p78.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 4, 1, '2022-10-31 13:06:27', '2022-10-31 13:06:27'),
-(12, 'DirtyCoins Print Cardigan - Ivory/Green', 490000, 15, 'uploads/p37.png', 'uploads/p39.png', 'uploads/p40.png', 'uploads/p38.png', 'uploads/p41.png', 'uploads/p42.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 2, '2022-11-05 21:03:16', '2022-11-05 21:03:16'),
-(13, 'DirtyCoins x LilWuyn Print L/S T-Shirt - Tan', 550000, 10, 'uploads/p19.png', 'uploads/p21.png', 'uploads/p22.png', 'uploads/p20.png', 'uploads/p23.png', 'uploads/p24.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 3, '2022-11-05 21:07:54', '2022-11-05 21:07:54'),
-(14, 'DirtyCoins x LilWuyn Basic T-Shirt - Silver Lining', 699000, 10, 'uploads/1.png', 'uploads/3.png', 'uploads/4.png', 'uploads/2.png', 'uploads/5.png', 'uploads/6.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 2, 3, '2022-11-07 08:44:17', '2022-11-07 08:44:17'),
-(15, 'DirtyCoins Twin Flowers T-shirt - White', 420000, 20, 'uploads/7.png', 'uploads/9.png', 'uploads/10.png', 'uploads/8.png', 'uploads/11.png', 'uploads/12.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 2, 1, '2022-11-07 08:48:39', '2022-11-07 08:48:39');
+(1, 'DC x OP Luffy Raglan Hoodie - Multicolor Special', 750000, 10, 'uploads/p1.png', 'uploads/p2.png', 'uploads/p3.png', 'uploads/p4.png', 'uploads/p5.png', 'uploads/p6.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 1, '2025-05-30 10:37:18', '2025-05-27 15:24:13'),
+(2, 'DC x OP Brook Hoodie - Blue', 699000, 20, 'uploads/p7.png', 'uploads/p8.png', 'uploads/p9.png', 'uploads/p12.png', 'uploads/p11.png', 'uploads/p10.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 1, '2025-05-30 15:11:49', '2025-05-27 15:11:49'),
+(3, 'DC x OP Luffy Attack T-shirt - White', 420000, 20, 'uploads/p13.png', 'uploads/p18.png', 'uploads/p15.png', 'uploads/p14.png', 'uploads/p17.png', 'uploads/p18.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 2, 1, '2025-05-30 17:31:22', '2025-05-27 12:09:25'),
+(4, 'DirtyCoins Don\'t Test Us Hoodie - Black', 648000, 20, 'uploads/p31.png', 'uploads/p32.png', 'uploads/p33.png', 'uploads/p36.png', 'uploads/p35.png', 'uploads/p34.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 2, '2025-05-30 21:59:06', '2025-05-27 15:24:13'),
+(5, 'DC x OP Chopper Hoodie - Black', 699000, 10, 'uploads/p67.png', 'uploads/p71.png', 'uploads/p69.png', 'uploads/p68-1.png', 'uploads/p70.png', 'uploads/p72.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 1, '2025-05-30 22:10:40', '2025-05-27 15:24:13'),
+(6, 'DirtyCoins x LilWuyn Logo Shorts - Black', 450000, 10, 'uploads/p25.png', 'uploads/p26.png', 'uploads/p27.png', 'uploads/p28.png', 'uploads/p29.png', 'uploads/p30.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 3, 3, '2025-05-30 12:37:46', '2025-05-27 08:41:12'),
+(7, 'Dico Wavy Shorts - Black', 490000, 20, 'uploads/p55.png', 'uploads/p56.png', 'uploads/p57.png', 'uploads/p58.png', 'uploads/p59.png', 'uploads/p60.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 3, 2, '2025-05-30 12:40:58', '2025-05-27 08:41:12'),
+(8, 'Shorts - Green', 450000, 20, 'uploads/p61.png', 'uploads/p62.png', 'uploads/p63.png', 'uploads/p64.png', 'uploads/p65.png', 'uploads/p66.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 3, 2, '2025-05-31 12:43:18', '2025-05-31 12:43:18'),
+(9, 'Cargo Pants - Black', 620000, 15, 'uploads/p43.png', 'uploads/p44.png', 'uploads/p45.png', 'uploads/p46.png', 'uploads/p47.png', 'uploads/p48.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 4, 2, '2025-05-31 12:55:56', '2025-05-31 12:55:56'),
+(10, 'DirtyCoins Dico Starry Jogger Pants - Red', 490000, 20, 'uploads/p49-1.png', 'uploads/p50.png', 'uploads/p51.png', 'uploads/p52.png', 'uploads/p53.png', 'uploads/p54.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 4, 2, '2025-05-31 12:59:42', '2025-05-31 12:59:42'),
+(11, 'DC x OP Logo Print Khaki Pants - Black', 620000, 10, 'uploads/p73.png', 'uploads/p74.png', 'uploads/p75.png', 'uploads/p76.png', 'uploads/p77.png', 'uploads/p78.png', 'Chất liệu vải sợi tổng hợp. Hình mặt trước quần và logo DirtyCoins áp dụng kĩ thuật in lụa, lưng thun, có túi hai bên. Túi quần sau có nắp kèm nút bấm. Logo DirtyCoins thêu trên túi.', 4, 1, '2025-05-31 13:06:27', '2025-05-31 13:06:27'),
+(12, 'DirtyCoins Print Cardigan - Ivory/Green', 490000, 15, 'uploads/p37.png', 'uploads/p39.png', 'uploads/p40.png', 'uploads/p38.png', 'uploads/p41.png', 'uploads/p42.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 2, '2025-05-31 21:03:16', '2025-05-31 21:03:16'),
+(13, 'DirtyCoins x LilWuyn Print L/S T-Shirt - Tan', 550000, 10, 'uploads/p19.png', 'uploads/p21.png', 'uploads/p22.png', 'uploads/p20.png', 'uploads/p23.png', 'uploads/p24.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 1, 3, '2025-05-31 21:07:54', '2025-05-31 21:07:54'),
+(14, 'DirtyCoins x LilWuyn Basic T-Shirt - Silver Lining', 699000, 10, 'uploads/1.png', 'uploads/3.png', 'uploads/4.png', 'uploads/2.png', 'uploads/5.png', 'uploads/6.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 2, 3, '2025-05-31 08:44:17', '2025-05-31 08:44:17'),
+(15, 'DirtyCoins Twin Flowers T-shirt - White', 420000, 20, 'uploads/7.png', 'uploads/9.png', 'uploads/10.png', 'uploads/8.png', 'uploads/11.png', 'uploads/12.png', 'Chất liệu nỉ cotton. Hình in thân trước và tay áo áp dụng công nghệ in kéo lụa thủ công. Bo cổ tay và bo thân trùng với màu vải tay áo và thân áo tương ứng. Tag vải riêng của BST được may trên túi phía trước.', 2, 1, '2025-05-31 08:48:39', '2025-05-31 08:48:39');
 
 -- --------------------------------------------------------
 
@@ -215,7 +215,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `fullname`, `tendangnhap`, `email`, `diachi`, `matkhau`, `dienthoai`, `role`) VALUES
-(1, 'Chu Chu', 'Admin_Chu', 'admin@gmail.com', 'TP.HCM', '9999', 123456789, 'admin'),
+(1, 'Admin_', 'Admin_luxury', 'admin@gmail.com', 'TP.HCM', '9999', 123456789, 'admin'),
 (2, 'User', 'user_luxury', 'userluxury@gmail.com', 'TPHCM', '9999', 123452289, 'user');
 
 --
